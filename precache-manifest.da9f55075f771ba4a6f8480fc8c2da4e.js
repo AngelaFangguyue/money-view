a4985eaf5b089c419cf121bbde81f298c7ac2b2a
AngelaFang <1823476439@qq.com>
2020-04-06 22:14:28 +0800

    deploy

---
+++ precache-manifest.da9f55075f771ba4a6f8480fc8c2da4e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73bea43e8a8ff837c764",
+    "url": "/money-view/css/about.9bb3aede.css"
+  },
+  {
+    "revision": "78f9161cea8686ae0a93",
+    "url": "/money-view/css/app.d8c3e488.css"
+  },
+  {
+    "revision": "7facd5678a5ed35ae9aea8da59848291",
+    "url": "/money-view/index.html"
+  },
+  {
+    "revision": "73bea43e8a8ff837c764",
+    "url": "/money-view/js/about.245f59b4.js"
+  },
+  {
+    "revision": "78f9161cea8686ae0a93",
+    "url": "/money-view/js/app.a55b8aa7.js"
+  },
+  {
+    "revision": "86cde7e2c080594a6d8b",
+    "url": "/money-view/js/chunk-vendors.1dcb2143.js"
+  },
+  {
+    "revision": "ce03c41b4ede30f0c8b860324a3f6a34",
+    "url": "/money-view/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-view/robots.txt"
+  }
+]);
